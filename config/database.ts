cd2b35af2da228c0614ec308c6a9e2fe986cdc58
hrishivarun <70848751+hrishivarun@users.noreply.github.com>
2024-10-14 00:23:52 +0530

    build failing on render

--- config/database.ts
+++ config/database.ts
@@ -4,11 +4,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 let db_key = process.env.DATABASE_KEY || "mQamLcw718irHVvmV6SKOQ5Sh0Xzs1Oi";
 
-const sequelize = new Sequelize('bitespeed_db_912b', 'root', db_key, {
-    host: 'postgresql://root:mQamLcw718irHVvmV6SKOQ5Sh0Xzs1Oi@dpg-cs5vnjt6l47c73f90ca0-a/bitespeed_db_912b',
-    dialect: 'postgres',
-    port: 5432
+
+// Connection string for PostgreSQL hosted on Render
+const sequelize = new Sequelize('postgresql://root:mQamLcw718irHVvmV6SKOQ5Sh0Xzs1Oi@dpg-cs5vnjt6l47c73f90ca0-a.frankfurt-postgres.render.com/bitespeed_db_912b', {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  port: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true, // Enforce SSL connection
+      rejectUnauthorized: false, // You may need this if Render requires a self-signed certificate
+    },
+  },
+  logging: false, // Disable logging if not needed
 });
+
+// const sequelize = new Sequelize('bitespeed_db_912b', 'root', db_key, {
+//     host: 'postgresql://root:mQamLcw718irHVvmV6SKOQ5Sh0Xzs1Oi@dpg-cs5vnjt6l47c73f90ca0-a/bitespeed_db_912b',
+//     dialect: 'postgres',
+//     port: 5432
+// });
 
 sequelize.authenticate()
     .then(() => {
